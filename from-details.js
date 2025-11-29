0a3b8489b13e4b8327d5ae25bbe092fc210f1ee0
Sundesh280 <albertghimire280@gmail.com>
2025-11-29 10:25:20 +0545

    given comment to make esay for understand

--- from-details.js
+++ from-details.js
@@ -1,3 +1,4 @@
+// save in local strogae in new tab 
 document.getElementById("n").textContent = localStorage.getItem("name");
 document.getElementById("e").textContent = localStorage.getItem("email");
 document.getElementById("m").textContent = localStorage.getItem("message");
